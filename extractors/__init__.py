2d3f108e0fe1232cc247438e8252553462873804
Apoorva Kolhatkar <apokol@umich.edu>
2026-01-13 00:59:37 -0500

    Update __init__.py

--- extractors/__init__.py
+++ extractors/__init__.py
@@ -1,3 +1,4 @@
+# extractors/__init__.py
 from typing import List
 
 from models import SectionedNote, Candidate
@@ -8,22 +9,12 @@ from .comorbidities import extract_comorbidities
 from .procedures import extract_reconstruction, extract_lymph_node_mgmt
 from .pbs import extract_pbs
 from .mastectomy import extract_mastectomy
+from .age import extract_age   # ← NEW
 
 
 def extract_all(sec: SectionedNote) -> List[Candidate]:
     """
     Run all extractors and return a flat list of candidates.
-
-    Tier 1:
-      - BMI
-      - SmokingStatus
-      - Comorbidities (DM, HTN, Cardiac, VTE, Steroid, CancerOther)
-      - Reconstruction (type, laterality, timing, performed/planned)
-      - Lymph node management
-
-    Tier 2:
-      - PBS_Lumpectomy / PBS_Other
-      - Mastectomy_Laterality / Mastectomy_Type / Mastectomy_Performed
     """
     cands = []  # type: List[Candidate]
 
@@ -33,6 +24,9 @@ def extract_all(sec: SectionedNote) -> List[Candidate]:
     cands.extend(extract_comorbidities(sec))
     cands.extend(extract_reconstruction(sec))
     cands.extend(extract_lymph_node_mgmt(sec))
+
+    # Tier 1.5 – shared demographic variable
+    cands.extend(extract_age(sec))
 
     # Tier 2
     cands.extend(extract_pbs(sec))
