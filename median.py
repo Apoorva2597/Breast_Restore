c3c113b48b1ddaa251a4ff7a5f2cea076a1685d7
Apoorva Kolhatkar <apokol@umich.edu>
2026-01-28 17:58:09 -0500

    Update median.py

--- median.py
+++ median.py
@@ -1,7 +1,4 @@
 import pandas as pd
-
-df = pd.read_csv("patient_level_phase1_p50.csv")
-
-print("Chemo positive:", df["Chemo"].sum())
-print("Radiation positive:", df["Radiation"].sum())
-print("Total patients:", len(df))
+df=pd.read_csv('patient_level_phase1_p50.csv')
+print(df[['LymphNodeMgmt_Performed','LymphNodeMgmt_Type']].head())
+print('Performed non-null:', df['LymphNodeMgmt_Performed'].notnull().sum()); print('Type non-null:', df['LymphNodeMgmt_Type'].notnull().sum())"
