937ecc389f59b69497e09c8fb88c61a922092a38
Apoorva Kolhatkar <apokol@umich.edu>
2026-02-12 10:16:29 -0500

    Update byte.py

--- byte.py
+++ byte.py
@@ -1,4 +1,4 @@
-b=open('median.py','rb').read(); \
-import codecs; \
-try: b.decode('utf-8'); print('OK: valid utf-8'); \
+b=open('median.py','rb').read(); 
+import codecs; 
+try: b.decode('utf-8'); print('OK: valid utf-8'); 
 except UnicodeDecodeError as e: print('BAD utf-8:', e, 'byte=', hex(b[e.start]))
