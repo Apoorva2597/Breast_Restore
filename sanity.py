4e7cfb4a9f7ccbf848f19a9ebd6f247ef2331e8b
Apoorva Kolhatkar <apokol@umich.edu>
2026-02-20 17:35:01 -0500

    Update sanity.py

--- sanity.py
+++ sanity.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 df=pd.read_csv('stage1_complications_row_hits.csv')
-print(df[df['complication'].eq('Other (systemic)')]['snippet'].head(20).tolist())
+print(df[df['complication'].eq('Wound infection')]['snippet'].head(20).tolist())
