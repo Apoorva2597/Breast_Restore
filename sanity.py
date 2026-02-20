22f52ebadc181d00d21440dc27fb8b18de368c90
Apoorva Kolhatkar <apokol@umich.edu>
2026-02-20 17:33:19 -0500

    Update sanity.py

--- sanity.py
+++ sanity.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 df=pd.read_csv('stage1_complications_row_hits.csv')
-print(df['complication'].value_counts().head(20))
+print(df[df['complication'].eq('Other (systemic)')]['snippet'].head(20).tolist())
