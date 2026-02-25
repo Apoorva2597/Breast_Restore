92880b5d04467eb909d6c9c470c0ea55fcbee268
Apoorva Kolhatkar <apokol@umich.edu>
2026-02-25 01:38:55 -0500

    Update sanity.py

--- sanity.py
+++ sanity.py
@@ -1,1 +1,5 @@
-gold[(gold["Stage2_MinorComp"]==1) & (gold["Stage2_Reoperation"]==0) & (gold["Stage2_Rehospitalization"]==0)].head(10)
+import pandas as pd
+gold = pd.read_csv("gold_cleaned_for_cedar.csv")
+gold[(gold["Stage2_MinorComp"]==1) & 
+     (gold["Stage2_Reoperation"]==0) & 
+     (gold["Stage2_Rehospitalization"]==0)].head(10)
