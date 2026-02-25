ae135d78e74b766b6db87829c72d98de721fa679
Apoorva Kolhatkar <apokol@umich.edu>
2026-02-25 01:46:20 -0500

    Update sanity.py

--- sanity.py
+++ sanity.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 
+gold = "gold_cleaned_for_cedar.csv"
+
 print(gold[(gold["Stage2_MinorComp"]==1) & 
      (gold["Stage2_Reoperation"]==0) & 
      (gold["Stage2_Rehospitalization"]==0)][
