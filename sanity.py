ab18fdfd282f4a12ea80399381928a2ea54e8d66
Apoorva Kolhatkar <apokol@umich.edu>
2026-03-05 21:58:24 -0500

    Update sanity.py

--- sanity.py
+++ sanity.py
@@ -1,18 +1,9 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import pandas as pd
 
-MODEL_DIR="/home/apokol/Breast_Restore/bart_large_mnli"
+gold = pd.read_csv("/home/apokol/Breast_Restore/_outputs/patient_master.csv", dtype=str)
+pred = pd.read_csv("/home/apokol/Breast_Restore/gold_cleaned_for_cedar.csv", dtype=str)
 
-tok = AutoTokenizer.from_pretrained(MODEL_DIR, use_fast=False)
-mdl = AutoModelForSequenceClassification.from_pretrained(MODEL_DIR)
-
-clf = pipeline(
-    "zero-shot-classification",
-    model=mdl,
-    tokenizer=tok,
-    device=-1
-)
-
-print(clf(
-    "The tissue expander was removed and replaced with a silicone implant.",
-    candidate_labels=["stage2 reconstruction","not stage2 reconstruction"]
-))
+print("GOLD COLUMNS:")
+print(list(gold.columns))
+print("\nPRED COLUMNS:")
+print(list(pred.columns))
