49cd1aebcd4a4aab184e4e1d2113052f4a3d3a95
Apoorva Kolhatkar <apokol@umich.edu>
2026-04-13 17:33:07 -0400

    Update sanity.py

--- sanity.py
+++ sanity.py
@@ -1,7 +1,9 @@
 import pandas as pd
-df = pd.read_csv('_outputs/master_abstraction_rule_FINAL_NO_GOLD_with_stage2_preds_complications.csv', dtype=str)
-cols = ['PBS_Breast Reduction','PBS_Mastopexy','PBS_Other','Stage2_MinorComp','Stage2_Rehospitalization']
+df = pd.read_csv('_outputs/master_abstraction_rule_FINAL_NO_GOLD.csv', dtype=str)
+cols = ['PBS_Breast Reduction','PBS_Mastopexy','PBS_Other']
 for c in cols:
     if c in df.columns:
         vc = df[c].value_counts(dropna=False)
         print(c, dict(vc))
+    else:
+        print(c, 'COLUMN MISSING')
