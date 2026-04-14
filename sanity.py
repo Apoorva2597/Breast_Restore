8580e8e8a6fb8dd046e344727f72415d45c238bb
Apoorva Kolhatkar <apokol@umich.edu>
2026-04-13 23:40:34 -0400

    Update sanity.py

--- sanity.py
+++ sanity.py
@@ -1,3 +1,5 @@
 import pandas as pd
-df = pd.read_csv('_frozen_stage2/20260228_200052/validation_merged.csv', dtype=str, nrows=5)
-print(df[['PRED_HAS_STAGE2','STAGE2_DATE','WINDOW_START']].to_string())
+df = pd.read_csv('_outputs/complications_patch_evidence.csv', dtype=str)
+s2 = df[df['STAGE_ASSIGNED'] == 'STAGE2']
+print('Total STAGE2 evidence rows:', len(s2))
+print(s2['FIELD'].value_counts().head(20))
