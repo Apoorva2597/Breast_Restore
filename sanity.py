f144995b515eb7e9398e066d4a38f80b8c42f59a
Apoorva Kolhatkar <apokol@umich.edu>
2026-04-26 13:27:44 -0400

    Update sanity.py

--- sanity.py
+++ sanity.py
@@ -1,4 +1,1 @@
-
-import pandas as pd
-df = pd.read_csv('_outputs/master_abstraction_rule_FINAL_NO_GOLD.csv', dtype=str)
-print('Unique MRNs:', df['MRN'].nunique())
+head -3 _outputs/master_abstraction_rule_FINAL_NO_GOLD_with_stage2_preds_complications.csv | tr ',' '\n' | head -60
